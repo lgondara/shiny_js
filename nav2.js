0655ec04d6a379fb395efd6b4bbf638498c1ac49
Lovedeep Gondara <lvdp00@gmail.com>
2021-04-26 14:57:16 -0700

    Add files via upload
    Added extra menu items including create your own table

--- nav2.js
+++ nav2.js
@@ -4,8 +4,12 @@ $(document).ready(function() {
     $( this ).addClass("dropdown-menu-" + num);
   });
   
-  $(".dropdown-menu-1").append('<li><a href="https://google.com" target="_blank">Incidence report 1</a></li>');
-  $(".dropdown-menu-1").append('<li><a href="https://google.com" target="_blank">Incidence report 2</a></li>');
-  $(".dropdown-menu-2").append('<li><a href="https://google.com" target="_blank">Mortality report 1</a></li>');
-  $(".dropdown-menu-2").append('<li><a href="https://google.com" target="_blank">Mortality report 2</a></li>');
+  $(".dropdown-menu-1").append('<li><a href="https://google.com" target="_blank">New Cancer Diagnosis</a></li>');
+  $(".dropdown-menu-1").append('<li><a href="https://google.com" target="_blank">Crude Rate</a></li>');
+  $(".dropdown-menu-2").append('<li><a href="https://google.com" target="_blank">Mortality Counts</a></li>');
+  $(".dropdown-menu-2").append('<li><a href="https://google.com" target="_blank">Mortality Crude Rate</a></li>');
+  $(".dropdown-menu-3").append('<li><a href="https://google.com" target="_blank">Prevalence Report 1</a></li>');
+  $(".dropdown-menu-3").append('<li><a href="https://google.com" target="_blank">Prevalence Report 2</a></li>');
+  $(".dropdown-menu-4").append('<li><a href="https://google.com" target="_blank">New Cancer Diagnosis</a></li>');
+  $(".dropdown-menu-4").append('<li><a href="https://google.com" target="_blank">Mortality Counts</a></li>');
 });
